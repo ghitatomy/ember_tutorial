4f543f4908628af4f8f7fa69d5342b1e074cc22c
ghitatomy <ghita@luminos.software>
2019-01-07 10:16:40 +0200

    Add form and get data on action

---
+++ app/controllers/tasks/new.js
@@ -0,0 +1,12 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  actions: {
+    addTask: function() {
+      let title = this.get('title');
+      // let description = this.get('description');
+      // let date = this.get('date');
+      alert(title);
+    }
+  }
+});
